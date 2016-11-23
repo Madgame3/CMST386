68537c0195d300b6d1a25c56c34af62921b9aaa8
Stephen Harrington <harrington.st@gmail.com>
2016-11-23 21:18:06 +0900

    javascript time

---
+++ exercise3/javascript/convert.js
@@ -0,0 +1,4 @@
+function convertTemperature() {
+  alert("testing");
+  var convertType = prompt("Whats your name girl", "shorty");
+}
---
+++ exercise3/scripts/convert.js
@@ -0,0 +1,7 @@
+function Temperature(){
+  var choice = prompt("Enter '1' for F to C, Enter '2' for C to F", "1 or 2");
+    if(!choice == 1 || 2){
+      alert("You did not Enter 1 or 2, please try again");
+      Temperature();
+    }
+}
