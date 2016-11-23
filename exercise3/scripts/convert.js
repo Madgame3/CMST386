d41e0a688b272892353898487a56dae38f0cbdf8
Stephen Harrington <harrington.st@gmail.com>
2016-11-23 21:43:36 +0900

    javascript time

--- exercise3/scripts/convert.js
+++ exercise3/scripts/convert.js
@@ -1,12 +1,10 @@
 function Temperature(){
   var choice = prompt("Enter '1' for F to C, Enter '2' for C to F", "1 or 2");
-    if(choice==1){
+    if (choice == 1) {
       alert("you entered 1 for F to C");
-    }
-    else if (choice==2){
+    } else if (choice == 2) {
       alert("you entered 2 for C to F")
-    }
-    else{
+    } else{
       alert("you did not enter 1 or 2, please try again.")
       Temperature();
     }
