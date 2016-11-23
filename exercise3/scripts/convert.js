73f7da6b5b1023984b0186f1507cebe967ddbbb2
Stephen Harrington <harrington.st@gmail.com>
2016-11-24 01:28:38 +0900

    changes

--- exercise3/scripts/convert.js
+++ exercise3/scripts/convert.js
@@ -1,11 +1,8 @@
 function convertType(){
-  var choice = prompt("Enter '1' for Fahrenheit to Celsius, Enter '2' for Celsius to Fahrenheit", "1 or 2");
+  var choice = prompt("Enter '1' for F to C, Enter '2' for C to F", "1 or 2");
     if (choice == 1) {
-      alert("you entered 1 for Fahrenheit to Celsius");
       converttoC();
-
     } else if (choice == 2) {
-      alert("you entered 2 for Celsius to Fahrenheit")
       converttoF();
     } else{
       alert("you did not enter 1 or 2, please try again.")
