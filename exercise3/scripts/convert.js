cc28de0c876cde2d147159b5db8864a4c48b84d4
Stephen Harrington <harrington.st@gmail.com>
2016-11-23 22:10:40 +0900

    javascript time

--- exercise3/scripts/convert.js
+++ exercise3/scripts/convert.js
@@ -2,9 +2,11 @@ function convertType(){
   var choice = prompt("Enter '1' for F to C, Enter '2' for C to F", "1 or 2");
     if (choice == 1) {
       alert("you entered 1 for F to C");
+      converttoC();
 
     } else if (choice == 2) {
       alert("you entered 2 for C to F")
+      converttoF();
     } else{
       alert("you did not enter 1 or 2, please try again.")
       Temperature();
@@ -14,7 +16,7 @@ function converttoF(){
   ctemp = prompt("Enter the temperature in Celsius you want to convert to Fahrenheit", "0");
   if (!isNan(ctemp)) {
       alert("The value you entered is not a number, please try again.")
-      fucntion converttoF();
+      converttoF();
   }
   else {
     var ftemp = (ctemp*1.8) + 32;
@@ -22,6 +24,14 @@ function converttoF(){
   }
 }
 function converttoC(){
-
+  ftemp = prompt("Enter the temperature in Fahrenheit you want to convert to Celsuis", "0");
+  if (!isNan(ftemp)) {
+      alert("The value you entered is not a number, please try again.")
+       converttoC();
+  }
+  else {
+    var ctemp = (ftemp - 32) * .5556;
+    alert("Celsius: " + ctemp);
+  }
 
 }
