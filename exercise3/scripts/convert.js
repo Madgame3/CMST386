797c74b1b13a1461f1552a0a369dc2e41ffe13b8
Stephen Harrington <harrington.st@gmail.com>
2016-11-23 22:18:40 +0900

    javascript time

--- exercise3/scripts/convert.js
+++ exercise3/scripts/convert.js
@@ -26,7 +26,7 @@ function converttoF(){
 function converttoC(){
   ftemp = prompt("Enter the temperature in Fahrenheit you want to convert to Celsuis", "0");
   if (isNaN(ftemp)) {
-      alert(ftemp " is not a number, please try again.")
+      alert(ftemp + " is not a number, please try again.")
        converttoC();
   }
   else {
