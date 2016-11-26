b34ea5b3cbb510896b510a224400fcaf8144a43c
Stephen Harrington <harrington.st@gmail.com>
2016-11-26 22:50:28 +0900

    javascript coming out you

--- exercise4/scripts/ssn.js
+++ exercise4/scripts/ssn.js
@@ -2,4 +2,15 @@ function allowSSN(){
   var citizen = document.getElementById("citizen");
   var value = citizen.value;
   console.log("citizen value :" + value);
+  var ssn = document.getElementById("ssn");
+  if(value == 1){
+    ssn.disabled = false;
+    ssn.required = true;
+    console.log("ssn required");
+  }
+  else{
+    ssn.disabled = true;
+    ssn.required = false;
+    console.log("ssn disabled");
+  }
 }
