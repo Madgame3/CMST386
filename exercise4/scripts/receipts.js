b2f84c4853c0a77d526b9e35c7cdbdfd5f5e2731
Stephen Harrington <harrington.st@gmail.com>
2016-11-26 21:24:37 +0900

    javascript coming out you

---
+++ exercise4/scripts/receipts.js
@@ -0,0 +1,6 @@
+function addReceipts(x){
+  for(var i=0;i>x;i++){
+
+  }
+
+}
---
+++ exercise4/scripts/ssn.js
@@ -0,0 +1,5 @@
+function ssn(){
+  var citizen = document.getElementById("citizen");
+  var value = citizen.value;
+  console.log("citizen value :" + $value);
+}
