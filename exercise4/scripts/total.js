7c4549746f3d1cec202dd957fc4a9440632e3858
Stephen Harrington <harrington.st@gmail.com>
2016-11-28 10:26:35 +0900

    bug fixes

--- exercise4/scripts/total.js
+++ exercise4/scripts/total.js
@@ -3,23 +3,12 @@ function calculateTotal(){
   var tax = document.getElementById('taxes');
   var totals = document.getElementById('total');
   var receipts = document.getElementById('receiptsum');
-  console.log("calculateTotal was called");
   var taxable = 0;
   tax.value = 0;
   totals.value = 0;
-  if(isNaN(receipts.value)){
-    console.log("receipts is not a number");
-  }
-  if(isNaN(sub.value)){
-    console.log("sub is not a number");
-  }
   if((sub.value - receipts.value) > 0){
-    console.log("receipts value: " + receipts.value);
-    console.log("sub value: " + sub.value);
     taxable = sub.value - receipts.value;
-    console.log("taxable: " + taxable);
     tax.value = Math.round(taxable * .1);
-    console.log("tax value: " + tax.value);
     totals.value = sub.value - tax.value;
   }
   else{
