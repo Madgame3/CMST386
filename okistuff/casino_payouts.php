89b6f4aba6e5882665e8876716b2f6ee39e92e20
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 14:48:49 +0900

    first php test

--- okistuff/casino_payouts.php
+++ okistuff/casino_payouts.php
@@ -1,12 +1,7 @@
 <?php
     require "connect.php";
-    //form variables
-    $fname = htmlspecialchars($_POST['fname']);
-    $lname = htmlspecialchars($_POST['lname']);
-    $winnings = htmlspecialchars($_POST['winnings']);
-
     $sql = "Select * from casino";
-    $results = $conn->qeury($sql)
+    $results = $conn->qeury($sql);
       echo $results;
     $conn->close();
 ?>
