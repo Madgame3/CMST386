c9e814ec1d9f1fb22c71b9c359efaac13265cdcf
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 14:24:36 +0900

    first php test

--- okistuff/casino_post.php
+++ okistuff/casino_post.php
@@ -1,18 +1,5 @@
 <?php
-
-  $db_host = "localhost";
-  $db_username = "okistuff";
-  $db_pass = "password";
-  $db_name = "okistuff";
-
-  $conn = new mysqli($db_host, $db_username, $db_pass, $db_name);
-  if($conn->connect_error){
-    die("Connection failed:" . $conn->connect_error);
-  }
-  else{
-      echo "Connect to Database was Successful" . "<br>";
-  }
-
+  require "connect.php";
   $fname = htmlspecialchars($_POST['fname']);
   $lname = htmlspecialchars($_POST['lname']);
   $winnings = htmlspecialchars($_POST['winnings']);
--- okistuff/connect.php
+++ okistuff/connect.php
@@ -4,11 +4,8 @@
   $db_pass = "password";
   $db_name = "okistuff";
 
-  $conn = new mysqli("$db_host","$db_username","$db_pass", "$dbname");
+  $conn = new mysqli($db_host, $db_username, $db_pass, $db_name);
   if($conn->connect_error){
     die("Connection failed:" . $conn->connect_error);
-  }
-  else{
-      echo "Connect to Database was Successful";
   }
  ?>
