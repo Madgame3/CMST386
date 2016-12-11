d7042460d92771f77f5c5f756a6504bf654a28e4
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 11:56:54 +0900

    first php test

--- okistuff/mariadb_connect.php
+++ okistuff/mariadb_connect.php
@@ -6,6 +6,4 @@
 
   @mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to SQL, check your stuff Steve");
   @mysql_select_db("$db_name") or die ("No Database, check your stuff Steve");
-
-  echo "Successful Connection";
  ?>
