d44e66a173179eab7ede839d91a79370c3beb418
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 14:56:35 +0900

    first php test

--- okistuff/casino_payouts.php
+++ okistuff/casino_payouts.php
@@ -2,5 +2,12 @@
     require "connect.php";
     $sql = "Select * from casino";
     $results = $conn->query($sql);
-    echo $results;
+    if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "First Name: " . $row["fname"]. " - Last Name: " . $row["lname"]. "- Winnings: " . $row["winnings"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
 ?>
