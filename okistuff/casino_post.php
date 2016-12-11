3c67fd9aa46eea26bc1e63da267f904a06dcb31a
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 15:13:34 +0900

    first php test

---
+++ okistuff/casino_post.php
@@ -0,0 +1,14 @@
+<?php
+    require "connect.php";
+    //form variables
+    $fname = htmlspecialchars($_POST['fname']);
+    $lname = htmlspecialchars($_POST['lname']);
+    $winnings = htmlspecialchars($_POST['winnings']);
+
+    $sql = "INSERT INTO casino (fname, lname, winnings) VALUES ('$fname', '$lname', '$winnings')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error . $sql->error;
+    }
+?>
