55afe3dea0ef27564d0bff5a836e2a3c2a97aba7
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 12:09:43 +0900

    first php test

--- okistuff/casino_post.php
+++ okistuff/casino_post.php
@@ -3,14 +3,10 @@
 
   $fname = htmlspecialchars($_POST['fname']);
   $lname = htmlspecialchars($_POST['lname']);
-  $citizen = htmlspecialchars($_POST['citizen']);
-  $ssn = htmlspecialchars($_POST['ssn']);
   $winnings = htmlspecialchars($_POST['winnings']);
 
   echo "Your information has been saved to our database";
   echo $fname;
   echo $lname;
-  echo $citizen;
-  echo $ssn;
   echo $winnings;
  ?>
