58966bbe09d561b517c368d9675b689a366f03ad
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 13:10:09 +0900

    first php test

--- okistuff/casino_post.php
+++ okistuff/casino_post.php
@@ -1,5 +1,16 @@
 <?php
-  require 'mariadb_connect.php';
+  $db_host = "localhost";
+  $db_username = "okistuff";
+  $db_pass = "password";
+  $db_name = "okistuff";
+
+  $conn = new mysqli("$db_host","$db_username","$db_pass", "$dbname");
+  if($conn->connect_error){
+    die("Connection failed:" . $conn->connect_error);
+  }
+  else{
+      echo "Connect to Database was Successful" . "<br>";
+  }
 
   $fname = htmlspecialchars($_POST['fname']);
   $lname = htmlspecialchars($_POST['lname']);
@@ -21,6 +32,5 @@
   } else {
       echo "Error creating table: " . $conn->error;
   }
-
   $conn->close();
 ?>
