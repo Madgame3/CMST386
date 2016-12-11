189b0a6a9836d1ee5dbfd513acbb8db7dc5b9f73
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 15:08:04 +0900

    first php test

--- okistuff/casino_payouts.php
+++ okistuff/casino_payouts.php
@@ -1,13 +1,23 @@
 <?php
     require "connect.php";
     $sql = "select fname, lname, winnings from casino";
-    $results = $conn->query($sql);
-    if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "First Name: " . $row["fname"]. " - Last Name: " . $row["lname"]. "- Winnings: " . $row["winnings"]. "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
+    $result = mysqli_query($conn,"SELECT * FROM casino");
+
+  echo "<table border='1'>
+  <tr>
+  <th>Firstname</th>
+  <th>Lastname</th>
+  <th>Winnings</th>
+  </tr>";
+
+  while($row = mysqli_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['fname'] . "</td>";
+  echo "<td>" . $row['lname'] . "</td>";
+  echo "<td>" . $row['winnings'] . "</td>";
+  echo "</tr>";
+  }
+  echo "</table>";
+  mysqli_close($conn);
 ?>
