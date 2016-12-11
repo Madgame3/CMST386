dc87e8b5f762ec015552abba09db83faafa73924
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 14:45:52 +0900

    first php test

--- okistuff/casino_post.php
+++ okistuff/casino_post.php
@@ -9,6 +9,6 @@
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     } else {
-    }
         echo "Error: " . $sql . "<br>" . $conn->error . $sql->error;
+    }
 ?>
