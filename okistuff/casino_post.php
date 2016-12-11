bac8c35177c6acf538e1d6134284352bc51d07c2
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 13:08:44 +0900

    first php test

--- okistuff/casino_post.php
+++ okistuff/casino_post.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = new mysqli("$db_host","$db_username","$db_pass", "$dbname");
+  require 'mariadb_connect.php';
 
   $fname = htmlspecialchars($_POST['fname']);
   $lname = htmlspecialchars($_POST['lname']);
@@ -7,8 +7,20 @@
 
   echo "<br>" . $fname . "<br>" . $lname ."<br>". $winnings;
   echo "<br>" . '<a href="../okistuff/casino.html">Casino</a>';
+  echo "<br>" . $conn;
+  #$sql = "INSERT INTO casino (fname, lname, winnings) VALUES ($fname, $lname, $winnings)";
+  $sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP
+  )";
+  if ($conn->query($sql) === TRUE) {
+      echo "Table MyGuests created successfully";
+  } else {
+      echo "Error creating table: " . $conn->error;
+  }
 
-  $sql = "INSERT INTO casino (fname, lname, winnings) VALUES ($fname, $lname, $winnings)";
-  mysqli_query($sql, $conn);
-  mysqli_close($conn);
+  $conn->close();
 ?>
