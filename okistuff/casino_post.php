dc8398094d99b2e06d1601c9ad4674b2171787de
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 13:18:52 +0900

    first php test

--- okistuff/casino_post.php
+++ okistuff/casino_post.php
@@ -18,19 +18,7 @@
 
   echo "<br>" . $fname . "<br>" . $lname ."<br>". $winnings;
   echo "<br>" . '<a href="../okistuff/casino.html">Casino</a>';
-  echo "<br>" . $conn;
-  #$sql = "INSERT INTO casino (fname, lname, winnings) VALUES ($fname, $lname, $winnings)";
-  $sql = "CREATE TABLE MyGuests (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP
-  )";
-  if ($conn->query($sql) === TRUE) {
-      echo "Table MyGuests created successfully";
-  } else {
-      echo "Error creating table: " . $conn->error;
-  }
-  $conn->close();
+
+  $sql = "INSERT INTO casino VALUES ("$fname", "$lname", "$winnings")";
+  mysqli_query($sql);
 ?>
