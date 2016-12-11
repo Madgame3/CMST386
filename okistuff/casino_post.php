adc22c379673ba0f5b9bd231f45d6eb8746388e8
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 12:38:30 +0900

    first php test

--- okistuff/casino_post.php
+++ okistuff/casino_post.php
@@ -1,14 +1,3 @@
 <?php
   require 'mariadb_connect.php';
-
-  $fname = htmlspecialchars($_POST['fname']);
-  $lname = htmlspecialchars($_POST['lname']);
-  $winnings = htmlspecialchars($_POST['winnings']);
-
-  echo "Your information has been saved to our database";
-  echo $fname;
-  echo $lname;
-  echo $winnings;
-  $sql = "INSERT INTO casino (fname, lname, winnings)
-VALUES ("$fname", "$lname", "$winnings")";
  ?>
