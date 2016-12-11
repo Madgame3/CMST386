1a5a1a047a18a2c7f0802d04e2b7eb9e7f48c161
Stephen Harrington <harrington.st@gmail.com>
2016-12-11 12:06:45 +0900

    first php test

--- okistuff/casino_post.php
+++ okistuff/casino_post.php
@@ -1,12 +1,5 @@
 <?php
-
-  $db_host = "localhost";
-  $db_username = "okistuff";
-  $db_pass = "password";
-  $db_name = "okistuff";
-
-  @mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to SQL, check your stuff Steve");
-  @mysql_select_db("$db_name") or die ("No Database, check your stuff Steve");
+ require mariadb_connect.php
 
   $fname = htmlspecialchars($_POST['fname']);
   $lname = htmlspecialchars($_POST['lname']);
@@ -15,9 +8,9 @@
   $winnings = htmlspecialchars($_POST['winnings']);
 
   echo "Your information has been saved to our database";
-  echo $fname + "<br>";
-  echo $lname + "<br>";
-  echo $citizen + "<br>";
-  echo $ssn + "<br>";
-  echo $winnings + "<br>" ;
+  echo $fname;
+  echo $lname;
+  echo $citizen;
+  echo $ssn;
+  echo $winnings;
  ?>
