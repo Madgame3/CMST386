abbf2dc68e1965dfb517548d6e0a4c671da1ed5d
Stephen Harrington <harrington.st@gmail.com>
2016-12-13 15:43:36 +0900

    major changes

--- okistuff/contact.php
+++ okistuff/contact.php
@@ -9,10 +9,10 @@
    ?>
    <article>
      <div>
+       <h2>Contact Us</h2>
+       <p>We're Kind of lonely so we love it when we get to hear you're feedback</p>
      </div>
      <form id="contact-us" action="./contact_post.php" method="post">
-       <fieldset>
-       <legend>Contact Us</legend>
        First Name: <br>
        <input type="text" name="fname" required><br><br>
        Last Name: <br>
@@ -26,8 +26,17 @@
        Question/Comments<br>
        <textarea name="question" rows="10" cols="30" required></textarea><br><br>
        <button class="orange-button" type="submit">Submit</button>
-       </fieldset>
      </form>
+     <div>
+       <strong>Top 5 Things</strong>
+       <ol>
+         <li>Why are you so Awesome?</li>
+         <li>How can I be more like you?</li>
+         <li>OkiStuff rocks!!</li>
+         <li>Super Awesome!</li>
+         <li>Literally nothing works on this stupid site</li>
+       </ol>
+     </div>
    </article>
  </body>
 </html>
