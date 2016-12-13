b51369732064c528476ece08c3156849f33dbb82
Stephen Harrington <harrington.st@gmail.com>
2016-12-13 18:25:31 +0900

    major changes

--- okistuff/contact_post.php
+++ okistuff/contact_post.php
@@ -13,8 +13,25 @@
        $name = htmlspecialchars($_POST['name']);
        $email = htmlspecialchars($_POST['email']);
        $comment = htmlspecialchars($_POST['comment']);
-       echo "name: ".$name."<br>"."email: ". $email."<br>"."Comment:". $comment."<br>";
-       mysqli_close($conn);
+       $sql = "INSERT INTO comments (name, email, comment) VALUES ('$name', '$email', '$comment')";
+       if ($conn->query($sql) === TRUE) {
+           echo "<h1>New record created successfully</h1>";
+           echo "<table class='oki_table'>
+           <tr>
+           <th>Name</th>
+           <th>Email</th>
+           <th>Comment</th>
+           </tr>";
+           echo "<tr>";
+           echo "<td>" . $name . "</td>";
+           echo "<td>" . $email . "</td>";
+           echo "<td>" . $comment . "</td>";
+           echo "</tr>";
+           echo "</table>";
+       } else {
+           echo "Error: " . $sql . "<br>" . $conn->error . $sql->error;
+       }
+      mysqli_close($conn);
       ?>
    </section>
    <?php
