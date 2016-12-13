84eadba6aae92aa6234455a5e0a2ed21e3784975
Stephen Harrington <harrington.st@gmail.com>
2016-12-13 14:54:38 +0900

    major changes

--- okistuff/shop.php
+++ okistuff/shop.php
@@ -1,53 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./style/style.css">
-  <link rel="stylesheet" media="screen and (max-width:975px)" href="./style/style_975.css">
-  <link rel="stylesheet" media="screen and (max-width:650px)" href="./style/style_650.css">
-  <link rel="icon" href="./images/favicon.png">
-</head>
-  <title>OkiStuff.com</title>
-</head>
-<body>
-  <header>
-    <a href="./index.html"><img alt="brand" src="./images/logo1.png"></a>
-  </header>
-  <nav class="navbar">
-      <ul>
-      <li><a href="#">Shop by category</a></li>
-      <li><a href="#">Post an Item</a></li>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">How to Use</a></li>
-      <li><a href="#">Contact Us</a></li>
-      <!--
-      <li><a href="#">Clothing & Accessories</a></li>
-      <li><a href="#">Crafts & Tools</a></li>
-      <li><a href="#">Electronics</a></li>
-      <li><a href="#">Entertainment</a></li>
-      <li><a href="#">Home & Living</a></li>
-      <li><a href="#">Kids & Baby</a></li>
-      <li><a href="#">Pets</a></li>
-      <li><a href="#">Business & Services</a></li>
-      <li><a href="#">Vehicles</a></li>
-    -->
-    </ul>
-  </nav>
-  <div class="container">
-    <div class="inner-text">
-      <p>OkiStuff, helping you find whatever you need, whenever you need it!</p>
-    </div>
-    <div class="inner-search">
-      <form role="search" action="" method="post">
-        <input type="text" placeholder="Search">
-        <button type="submit">Submit</button>
-      </form>
-    </div> <!--inner-search-->
-  </div> <!-- container -->
-  <article>
-    <div><h1>Recent Items</h1></div>
-  </article>
-</body>
+<?php
+  require 'head.php';
+ ?>
+ <body>
+   <?php
+      require 'header.php';
+      require 'nav.php';
+      require 'container.php';
+   ?>
+   <article>
+     <h1>
+       New Items
+     </h1>
+     <ul>
+     <li><a href="#">Clothing & Accessories</a></li>
+     <li><a href="#">Crafts & Tools</a></li>
+     <li><a href="#">Electronics</a></li>
+     <li><a href="#">Entertainment</a></li>
+     <li><a href="#">Home & Living</a></li>
+     <li><a href="#">Kids & Baby</a></li>
+     <li><a href="#">Pets</a></li>
+     <li><a href="#">Business & Services</a></li>
+     <li><a href="#">Vehicles</a></li>
+     </ul>
+   </article>
+ </body>
 </html>
