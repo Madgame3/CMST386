befa55117754bd65330ec655a8d51d863a65f8f7
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 01:34:17 +0900

    item_post

--- okistuff/item_post.php
+++ okistuff/item_post.php
@@ -17,7 +17,7 @@
        $price = mysqli_real_escape_string($conn, $_POST['price']);
        $category = mysqli_real_escape_string($conn, $_POST['category']);
        $description = mysqli_real_escape_string($conn, $_POST['description']);
-       $image = file_get_contents($_FILES['image']['tmp_name']));
+       $image = file_get_contents($_FILES['image']['tmp_name']);
        $sql = "INSERT INTO items (title, image, category, description, contact_name, email, phone, price) VALUES ('$title','$image','$category','$description','$contact_name','$email','$phone','$price')";
        if ($conn->query($sql) === TRUE) {
            echo "<h1>New record created successfully</h1>";
