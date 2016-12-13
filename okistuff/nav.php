7842ee446577fdc8d77ae3c55bc9964ec67942fd
Stephen Harrington <harrington.st@gmail.com>
2016-12-13 21:39:38 +0900

    item_post

--- okistuff/nav.php
+++ okistuff/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
   <ul>
     <li><a href="./shop.php">Shop by category</a></li>
-    <li><a href="./postitem.php">Post an Item</a></li>
+    <li><a href="./item.php">Post an Item</a></li>
     <li><a href="./about.php">About Us</a></li>
     <li><a href="./howto.php">How to Use</a></li>
     <li><a href="./contact.php">Contact Us</a></li>
