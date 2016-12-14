b54b93e57bc5a711c58d513fc5c10b3b3f59d065
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 21:34:48 +0900

    table

--- okistuff/about.php
+++ okistuff/about.php
@@ -12,7 +12,7 @@
      <hr>
      <br>
      <p id="about-text">
-       Honestly, we're really not that interesting, but here are some thing that describe us...oh and by the way who's going to the midnight showing of Starwars!!!<br>
+       Honestly, we're really not that interesting, but here are some thing that describe us...oh and by the way who's going to the midnight showing of Starwars?????<br><br>
      <strong>Sure, we love puppies and sunny days and walks on the beach, but our real love is toiler paper.</strong><br><br>
      <strong>I never made a mistake in my life. I thought I did once, but I was wrong.  </strong><br><br>
      <strong>Life is like a very long TV show, without a remote control. </strong><br><br>
