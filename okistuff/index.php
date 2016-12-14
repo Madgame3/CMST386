28301da36cb0ba3e61d73cb4d803c2ea85896638
Stephen Harrington <harrington.st@gmail.com>
2016-12-15 00:34:44 +0900

    index

--- okistuff/index.php
+++ okistuff/index.php
@@ -18,13 +18,17 @@
         if($result->num_rows > 0){
           while($row = $result->fetch_assoc()){
             $image = stripslashes(file_get_contents($row['image']));
-            echo "<div class='item-box'>";
-            echo "<div class='item-box-title'>".$row['title']."</div>";
-            echo "<div class='item-box-image'>";
-            echo "<img src='data:image/png;base64,". $image ."' alt='item for sale'/>"."</div>";
-            echo "<div class='item-box-time'><p>".$row['price']."</p>".$row['date']."</div>";
-            echo "</div>";
-
+            if($image == NULL){
+                Echo "THIS ISN'T GOING TO Work";
+              }
+            else{
+              echo "<div class='item-box'>";
+              echo "<div class='item-box-title'>".$row['title']."</div>";
+              echo "<div class='item-box-image'>";
+              echo "<img src='data:image/png;base64,". $image ."' alt='item for sale'/>"."</div>";
+              echo "<div class='item-box-time'><p>".$row['price']."</p>".$row['date']."</div>";
+              echo "</div>";
+            }
             //"<img src='data:image/jpeg;base64',". base64_encode(stream_get_contents($row['image']))."/>";
           }
         }
