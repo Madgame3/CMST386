f5e79219084de743fa6badf473f2780020474989
Stephen Harrington <harrington.st@gmail.com>
2016-12-15 00:11:17 +0900

    index

--- okistuff/index.php
+++ okistuff/index.php
@@ -13,15 +13,17 @@
     <div id="item-flex-box">
       <?php
         require 'connect.php';
-        $sql = "select title,category,description,contact_name,phone,price,date from items order by date desc limit 16";
+        $sql = "select title,price,date,image from items order by date desc limit 16";
         $result = $conn->query($sql);
         if($result->num_rows > 0){
           while($row = $result->fetch_assoc()){
             echo "<div class='item-box'>";
             echo "<div class='item-box-title'>".$row['title']."</div>";
-            echo "<div class='item-box-image'>".'<img src="' .$blank. '" alt="Your Image" " />'."</div>";
-            echo "<div class='item-box-time'>".$row['date']."</div>";
+            echo "<div class='item-box-image'>"."<img src='data:image/jpeg;base64',". base64_encode(stream_get_contents($row['image']))."alt='item for sale'/>";."</div>";
+            echo "<div class='item-box-time'><p>".$row['price']."</p>".$row['date']."</div>";
             echo "</div>";
+
+            //"<img src='data:image/jpeg;base64',". base64_encode(stream_get_contents($row['image']))."/>";
           }
         }
         mysqli_close($conn);
