7a700231829347d7a9877303292c2545a84778dc
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 21:02:31 +0900

    sitemap

--- okistuff/scripts/item.js
+++ okistuff/scripts/item.js
@@ -1,5 +1,6 @@
 function itemCheck(){
   var category = document.getElementsByName("category");
-    if (category.value == "") {
-      alert("Please Select a Category")
+  if (category.value == "") {
+    alert("Please Select a Category")
   }
+}
