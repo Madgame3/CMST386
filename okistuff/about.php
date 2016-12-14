a65daf4b3525dda51ff2f509a1bd5c1a2b4a9571
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 14:57:59 +0900

    item_post

--- okistuff/about.php
+++ okistuff/about.php
@@ -9,7 +9,7 @@
    ?>
    <section>
      <h1>About Us - Funny???</h1>
-     <strong>SURE, WE LOVE PUPPIES AND SUNNY DAYS AND WALKS ON THE BEACH, BUT OUR REAL LOVE IS TOILET PAPER. WHY, YOU MIGHT ASK?</strong><br><br>
+     <strong>Sure, we love puppies and sunny days and walks on the beach, but our real love is toiler paper.</strong><br><br>
      <strong>I never made a mistake in my life. I thought I did once, but I was wrong.  </strong><br><br>
      <strong>Life is like a very long TV show, without a remote control. </strong><br><br>
      <strong>Math is fun, it teaches you life and death information, like when you're cold, you should go to a corner since it's 90 degrees there.</strong><br><br>
