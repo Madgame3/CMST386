62199aac4f739776d4aec4ce8a87e506e3963e36
Stephen Harrington <harrington.st@gmail.com>
2016-12-15 04:54:50 +0900

    index

--- okistuff/shop.php
+++ okistuff/shop.php
@@ -8,18 +8,34 @@
       require 'container.php';
    ?>
    <section id="index-page">
-     <h1>Category</h1>
+     <h1>Categories</h1>
      <hr>
      <div id="item-flex-box">
-       <div class="category-box"><a href="#">Clothing & Accessories</a></div>
-       <div class="category-box"><a href="#">Crafts & Tools</a></div>
-       <div class="category-box"><a href="#">Electronics</a></div>
-       <div class="category-box"><a href="#">Entertainment</a></div>
-       <div class="category-box"><a href="#">Home & Living</a></div>
-       <div class="category-box"><a href="#">Kids & Baby</a></div>
-       <div class="category-box"><a href="#">Pets</a></div>
-       <div class="category-box"><a href="#">Business & Services</a></div>
-       <div class="category-box"><a href="#">Vehicles</a></div>
+       <div class="category-box"><a href="#">Clothing & Accessories
+         <img src="./images/clothing.jpg" alt="clothing">
+       </a></div>
+       <div class="category-box"><a href="#">Crafts & Tools
+         <img src="./images/tools.jpg" alt="tools">
+       </a></div>
+       <div class="category-box"><a href="#">Electronics
+         <img src="./images/electronics.jpg" alt="electronics">
+       </a></div>
+       <div class="category-box"><a href="#">Hobbies & Sports
+         <img src="./images/sports.jpg" alt="sports">
+       </a></div>
+       <div class="category-box"><a href="#">Home & Living
+         <img src="./images/home.jpg" alt="home">
+       </a></div>
+       <div class="category-box"><a href="#">Kids & Baby
+       <img src="./images/baby.jpg" alt="baby"></a></div>
+       <div class="category-box"><a href="#">Pets
+         <img src="./images/pet.jpg" alt="pet">
+       </a></div>
+       <div class="category-box"><a href="#">Business & Services
+         <img src="./images/business.jpg" alt="business">
+       </a></div>
+       <div class="category-box"><a href="#">Vehicles
+       <img src="./images/vehicles.jpg" alt="car"></a></div>
      </div>
    </section>
    <?php
