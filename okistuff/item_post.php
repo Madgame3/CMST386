a31b49503724ede1154ee5d81705e087d8493611
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 11:26:18 +0900

    item_post

--- okistuff/item_post.php
+++ okistuff/item_post.php
@@ -27,7 +27,6 @@
 
        $sql = "INSERT INTO items (title, image, category, description, contact_name, email, phone, price) VALUES ('$title','$image','$category','$description','$contact_name','$email','$phone','$price')";
        if ($conn->query($sql) === TRUE) {
-           echo $image;
            echo "<h1>New record created successfully</h1>";
            echo "<table class='php_table'>
            <tr>
