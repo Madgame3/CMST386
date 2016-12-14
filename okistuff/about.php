07371cf01ad35f82b0826e44033c8d5af1c0bafe
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 22:07:23 +0900

    table

--- okistuff/about.php
+++ okistuff/about.php
@@ -13,13 +13,10 @@
      <br>
      <p class="about-text">
        Honestly, we're really not that interesting, but here are some thing that describe us...oh and by the way who's going to the midnight showing of Starwars?????</p>
-       <ul class="about-list-nostyle">
-         <li><p class="about-text">Sure, we love puppies and sunny days and walks on the beach, but our real love is toilet paper.</p></li>
-         <li><p class="about-text">I never made a mistake in my life. I thought I did once, but I was wrong.</p></li>
-         <li><p class="about-text">Life is like a very long TV show, without a remote control.</p></li>
-         <li><p class="about-text">Math is fun, it teaches you life and death information, like when you're cold, you should go to a corner since it's 90 degrees there.</p></li>
-       </ul>
-       <p>
+         <p class="about-text">Sure, we love puppies and sunny days and walks on the beach, but our real love is toilet paper.</p>
+         <p class="about-text">I never made a mistake in my life. I thought I did once, but I was wrong.</p>
+         <p class="about-text">Life is like a very long TV show, without a remote control.</p>
+         <p class="about-text">Math is fun, it teaches you life and death information, like when you're cold, you should go to a corner since it's 90 degrees there.</p>
     </section>
    <?php
       require 'footer.php';
