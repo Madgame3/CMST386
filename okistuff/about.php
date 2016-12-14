50fa5bdde29ad51c22959fdb3146010d6ea5687a
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 14:53:30 +0900

    item_post

--- okistuff/about.php
+++ okistuff/about.php
@@ -9,10 +9,10 @@
    ?>
    <section>
      <h1>About Us</h1>
-     <strong>SURE, WE LOVE PUPPIES AND SUNNY DAYS AND WALKS ON THE BEACH, BUT OUR REAL LOVE IS TOILET PAPER. WHY, YOU MIGHT ASK?</strong>
-     <strong>I never made a mistake in my life. I thought I did once, but I was wrong. - http://coolfunnyquotes.com </strong>
-     <strong>Life is like a very long TV show, without a remote control. - http://coolfunnyquotes.com </strong>
-     <strong>Math is fun, it teaches you life and death information, like when you're cold, you should go to a corner since it's 90 degrees there.</strong>
+     <strong>SURE, WE LOVE PUPPIES AND SUNNY DAYS AND WALKS ON THE BEACH, BUT OUR REAL LOVE IS TOILET PAPER. WHY, YOU MIGHT ASK?</strong><br>
+     <strong>I never made a mistake in my life. I thought I did once, but I was wrong. - http://coolfunnyquotes.com </strong><br>
+     <strong>Life is like a very long TV show, without a remote control. - http://coolfunnyquotes.com </strong><br>
+     <strong>Math is fun, it teaches you life and death information, like when you're cold, you should go to a corner since it's 90 degrees there.</strong><br>
     </section>
    <?php
       require 'footer.php';
