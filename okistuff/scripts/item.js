86db32d73656034b3093743c1411b47c975a87da
Stephen Harrington <harrington.st@gmail.com>
2016-12-15 03:21:54 +0900

    index

--- okistuff/scripts/item.js
+++ okistuff/scripts/item.js
@@ -1,24 +1,24 @@
 function itemCheck(){
-  var input, file;
-    if (!window.FileReader) {
-          console.log("The file API isn't supported on this browser yet.");
-        return;
-      }
-      input = document.getElementById('image');
-      if (!input) {
-          console.log("Um, couldn't find the fileinput element.");
-      }
-      else if (!input.files) {
-          console.log("This browser doesn't seem to support the `files` property of file inputs.");
-      }
-      else {
-        file = input.files[0];
-        if(file.size > 1048576 ){
-          alert("That File is Huge!, please compress and try again");
-          document.getElementsByName("submit").disabled = true;
-          }
-        else{
-          document.getElementsByName("submit").disabled = false;
-        }
-      }
+if (!window.FileReader) {
+      console.log("The file API isn't supported on this browser yet.");
+    return;
   }
+  input = document.getElementById('image');
+  if (!input) {
+      console.log("Um, couldn't find the fileinput element.");
+  }
+  else if (!input.files) {
+      console.log("This browser doesn't seem to support the `files` property of file inputs.");
+  }
+  else {
+    console.log("im doing something");
+    file = input.files[0];
+    if(file.size > 1048576 ){
+      alert("That File is Huge!, please compress and try again");
+      document.getElementsByName("submit").disabled = true;
+      }
+    else{
+      document.getElementsByName("submit").disabled = false;
+    }
+  }
+}
