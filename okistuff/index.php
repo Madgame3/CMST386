660db28f098a53d2fb3d3d0b02321cf0334ca186
Stephen Harrington <harrington.st@gmail.com>
2016-12-15 00:12:29 +0900

    index

--- okistuff/index.php
+++ okistuff/index.php
@@ -19,7 +19,7 @@
           while($row = $result->fetch_assoc()){
             echo "<div class='item-box'>";
             echo "<div class='item-box-title'>".$row['title']."</div>";
-            echo "<div class='item-box-image'>"."<img src='data:image/jpeg;base64',". base64_encode(stream_get_contents($row['image']))."alt='item for sale'/>";."</div>";
+            echo "<div class='item-box-image'>"."<img src='data:image/jpeg;base64',". base64_encode(stream_get_contents($row['image']))."alt='item for sale'/>"."</div>";
             echo "<div class='item-box-time'><p>".$row['price']."</p>".$row['date']."</div>";
             echo "</div>";
 
