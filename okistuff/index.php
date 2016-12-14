adb958f3f530ca46fcbca9e0fa0559996e70c767
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 23:18:57 +0900

    index

--- okistuff/index.php
+++ okistuff/index.php
@@ -17,8 +17,10 @@
         $result = $conn->query($sql);
         if($result->num_rows > 0){
           while($row = $result->fetch_assoc()){
+            echo "<div class='box'>";
             echo "title ".$row['title'];
-            echo "category: ".$row['category']."<br><br>";
+            echo "category: ".$row['category'];
+            echo "</div>";
 
           }
         }
