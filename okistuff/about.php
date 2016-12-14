8dfd9af3f95fc62b80bc6c1976e9c384f3d7ab80
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 21:33:20 +0900

    table

--- okistuff/about.php
+++ okistuff/about.php
@@ -8,11 +8,16 @@
       require 'container.php';
    ?>
    <section>
-     <h1>About Us - Funny???</h1>
+     <h1>About Us</h1>
+     <hr>
+     <br>
+     <p id="about-text">
+       Honestly, we're really not that interesting, but here are some thing that describe us...oh and by the way who's going to the midnight showing of Starwars!!!<br>
      <strong>Sure, we love puppies and sunny days and walks on the beach, but our real love is toiler paper.</strong><br><br>
      <strong>I never made a mistake in my life. I thought I did once, but I was wrong.  </strong><br><br>
      <strong>Life is like a very long TV show, without a remote control. </strong><br><br>
      <strong>Math is fun, it teaches you life and death information, like when you're cold, you should go to a corner since it's 90 degrees there.</strong><br><br>
+     <p>
     </section>
    <?php
       require 'footer.php';
