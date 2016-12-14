b09e23434f6ea235592acaea05def366d4c9b2d4
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 22:46:01 +0900

    table

--- okistuff/howto.php
+++ okistuff/howto.php
@@ -9,16 +9,17 @@
    ?>
    <section>
     <h1>The Ways Of OkiStuff</h1>
-    <p class="howto-text">OkiStuff is a local, free place to buy and sell stuff from your neighbors. The setup is similar to Craigslist, but more local (just your community), and more family friendly. Buyers and sellers meet in-person and exchange money and stuff directly. No shipping, no fees, no difficulties.<br>
+    <hr>
+    <p class="about-text">OkiStuff is a local, free place to buy and sell stuff from your neighbors. The setup is similar to Craigslist, but more local (just your community), and more family friendly. Buyers and sellers meet in-person and exchange money and stuff directly. No shipping, no fees, no difficulties.<br>
     </p>
-      <p><strong>How do I buy stuff from OkiStuff?</strong><br>
+      <p class="about-text"><strong>How do I buy stuff from OkiStuff?</strong><br>
       Browse by category<br>
       Search for an item<br>
       Select an item off the front page<br>
       View the item details<br>
       Feed your addiction by returning regularly.
       </p>
-      <p>
+      <p class="about-text">
       <strong>How do I sell my stuff on OkiStuff?</strong><br>
         Easy, just click post an item on the top navigation bar
     </p>
