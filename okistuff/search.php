47216b06a9ab584313edab8142427fe5418a88cb
Stephen Harrington <harrington.st@gmail.com>
2016-12-14 22:54:34 +0900

    table

--- okistuff/search.php
+++ okistuff/search.php
@@ -1,42 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./style.css">
-  <link rel="stylesheet" media="screen and (max-width:975px)" href="./style_975.css">
-  <link rel="stylesheet" media="screen and (max-width:650px)" href="./style_650.css">
-  <link rel="icon" href="./images/favicon.png">
-</head>
-  <title>OkiStuff.com</title>
-</head>
-<body>
-  <header>
-    <a href="./index.html"><img alt="brand" src="./images/logo1.png"></a>
-  </header>
-  <nav class="navbar">
-    <ul>
-      <li><a href="./shop.php">Shop by category</a></li>
-      <li><a href="./post.php">Post an Item</a></li>
-      <li><a href="./about.php">About Us</a></li>
-      <li><a href="./howto.php">How to Use</a></li>
-      <li><a href="./contact.php">Contact Us</a></li>
-    </ul>
-  </nav>
-  <div class="container">
-    <div class="inner-text">
-      <p>OkiStuff, helping you find whatever you need, whenever you need it!</p>
-    </div>
-    <div class="inner-search">
-      <form role="search" action="" method="post">
-        <input type="text" placeholder="Search">
-        <button type="submit">Submit</button>
-      </form>
-    </div> <!--inner-search-->
-  </div> <!-- container -->
-  <article>
-    <div><h1>Recent Items</h1></div>
-  </article>
-</body>
+<?php
+  require 'head.php';
+ ?>
+ <body>
+   <?php
+      require 'header.php';
+      require 'nav.php';
+      require 'container.php';
+   ?>
+   <section>
+     <h1>Search</h1>
+     <hr>
+     <p class=".about-text">Feature Coming Soon</p>
+
+   </section>
+   <?php
+      require 'footer.php';
+    ?>
+ </body>
 </html>
