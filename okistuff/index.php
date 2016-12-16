c1b211c3cfe648be1ab0f493403436dbc00c0030
Stephen Harrington <harrington.st@gmail.com>
2016-12-16 20:11:55 +0900

    styling

--- okistuff/index.php
+++ okistuff/index.php
@@ -19,7 +19,7 @@
           while($row = $result->fetch_assoc()){
             $image = base64_encode($row['image']);
               echo "<div class='item-box'>";
-              echo "<div class='item-box-title'>".$row['title']."</div>";
+              echo "<div class='item-box-title'><strong>".$row['title']."</strong></div>";
               echo "<div class='item-box-image'>";
               echo "<img src='data:image/png;base64,". $image ."' alt='item for sale'/>"."</div>";
               echo "<div class='item-box-time'><p> Price: ".$row['price']."</p></div>";
