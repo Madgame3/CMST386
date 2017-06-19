391c970c890fa2751edccff7b0d29666d4394e8a
Stephen Harrington <harrington.st@gmail.com>
2017-06-19 20:30:44 +0900

    login page

--- okistuff/login.php
+++ okistuff/login.php
@@ -1,7 +1,7 @@
 <?php
     $fb = new \Facebook\Facebook([
-    'app_id' => '{app-id}',
-    'app_secret' => '{app-secret}',
+    'app_id' => '{839841886165025}',
+    'app_secret' => '{517e3fdc7744d5b80c352ea17b6b95db}',
     'default_graph_version' => 'v2.9',
     //'default_access_token' => '{access-token}', // optional
     ]);
