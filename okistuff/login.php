fde935bfa72790de4bf1fd31495568c71d4e053d
Stephen Harrington <harrington.st@gmail.com>
2017-06-19 21:11:13 +0900

    login page

--- okistuff/login.php
+++ okistuff/login.php
@@ -1,9 +1,15 @@
 <?php
-    $fb = new Facebook\Facebook([
-    'app_id' => '839841886165025',
-    'app_secret' => '517e3fdc7744d5b80c352ea17b6b95db',
-    'default_graph_version' => 'v2.9',
-    ]);
+    #1. Installation
+       # a. Start session
+            session_start();
+        # b. Include the auto load file
+            require_once __DIR__.'../../vendor/autoload.php';
+        # c. Facebook Object with parameters 
+        $fb = new Facebook\Facebook([
+            'app_id' => '839841886165025',
+            'app_secret' => '517e3fdc7744d5b80c352ea17b6b95db',
+            'default_graph_version' => 'v2.9',
+        ]);
 
     $helper = $fb->getRedirectLoginHelper();
 
