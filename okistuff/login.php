c9f4e28c2ae29af2978539c252e33d3110a87997
Stephen Harrington <harrington.st@gmail.com>
2017-06-19 21:17:12 +0900

    login page

--- okistuff/login.php
+++ okistuff/login.php
@@ -13,8 +13,20 @@
 
     $helper = $fb->getRedirectLoginHelper();
 
-    $permissions = ['email']; // Optional permissions
-    $loginUrl = $helper->getLoginUrl('http://okistuff.com/fb-callback.php', $permissions);
+try {
+  // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+  // If you provided a 'default_access_token', the '{access-token}' is optional.
+  $response = $fb->get('/me', '{access-token}');
+} catch(\Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(\Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
 
-    echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+$me = $response->getGraphUser();
+echo 'Logged in as ' . $me->getName();
 ?>
