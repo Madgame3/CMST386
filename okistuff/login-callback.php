adb895bb6324c75dc5ddbf2dc3ff6d501aeae02d
Stephen Harrington <harrington.st@gmail.com>
2017-06-19 21:54:27 +0900

    login page

--- okistuff/login-callback.php
+++ okistuff/login-callback.php
@@ -1,5 +1,7 @@
 <?php
+    
     session_start();
+    require_once __DIR__ . '/vendor/autoload.php';
     $fb = new Facebook\Facebook([
         'app_id' => '839841886165025',
         'app_secret' => '517e3fdc7744d5b80c352ea17b6b95db',
