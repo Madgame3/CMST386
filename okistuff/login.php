a65fcb1c10e262ba8c9a77150018c8c55b3a8d58
Stephen Harrington <harrington.st@gmail.com>
2017-06-19 20:53:33 +0900

    login page

--- okistuff/login.php
+++ okistuff/login.php
@@ -1,31 +1,12 @@
-<?php
-    $fb = new \Facebook\Facebook([
-    'app_id' => '{839841886165025}',
-    'app_secret' => '{517e3fdc7744d5b80c352ea17b6b95db}',
-    'default_graph_version' => 'v2.9',
-    //'default_access_token' => '{access-token}', // optional
-    ]);
+$fb = new Facebook\Facebook([
+  'app_id' => '{839841886165025}',
+  'app_secret' => '{517e3fdc7744d5b80c352ea17b6b95db}',
+  'default_graph_version' => 'v2.9',
+  ]);
 
-    // Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
-    //   $helper = $fb->getRedirectLoginHelper();
-    //   $helper = $fb->getJavaScriptHelper();
-    //   $helper = $fb->getCanvasHelper();
-    //   $helper = $fb->getPageTabHelper();
+$helper = $fb->getRedirectLoginHelper();
 
-    try {
-    // Get the \Facebook\GraphNodes\GraphUser object for the current user.
-    // If you provided a 'default_access_token', the '{access-token}' is optional.
-    $response = $fb->get('/me', '{access-token}');
-    } catch(\Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-    } catch(\Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-    }
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
 
-    $me = $response->getGraphUser();
-    echo 'Logged in as ' . $me->getName();
-?>
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
