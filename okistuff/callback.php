07579c348b7c3fa806ba1f1ceb168f7fce865caf
Stephen Harrington <harrington.st@gmail.com>
2017-06-19 23:09:26 +0900

    login page

--- okistuff/callback.php
+++ okistuff/callback.php
@@ -10,7 +10,6 @@ echo 'can I see this';
 $helper = $fb->getRedirectLoginHelper();
 try {
   $accessToken = $helper->getAccessToken();
-  echo $accessToken;
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
   // When Graph returns an error
   echo 'Graph returned an error: ' . $e->getMessage();
@@ -18,6 +17,17 @@ try {
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
   // When validation fails or other local issues
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+if (isset($accessToken)) {
+  // Logged in!
+  $_SESSION['facebook_access_token'] = (string) $accessToken;
+  echo 'logged in';
+  // Now you can redirect to another page and use the
+  // access token from $_SESSION['facebook_access_token']
+  #header('Location: http://okistuff.com/index.php');    
+} elseif ($helper->getError()) {
+  // The user denied the request
   exit;
 }
 ?>
