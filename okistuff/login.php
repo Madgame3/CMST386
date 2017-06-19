d59f3c3340f5a85d804557fa461fd0d5bc65bcdb
Stephen Harrington <harrington.st@gmail.com>
2017-06-19 20:58:49 +0900

    login page

--- okistuff/login.php
+++ okistuff/login.php
@@ -1,12 +1,14 @@
-$fb = new Facebook\Facebook([
-  'app_id' => '839841886165025',
-  'app_secret' => '517e3fdc7744d5b80c352ea17b6b95db',
-  'default_graph_version' => 'v2.9',
-  ]);
+<?php
+    $fb = new Facebook\Facebook([
+    'app_id' => '839841886165025',
+    'app_secret' => '517e3fdc7744d5b80c352ea17b6b95db',
+    'default_graph_version' => 'v2.9',
+    ]);
 
-$helper = $fb->getRedirectLoginHelper();
+    $helper = $fb->getRedirectLoginHelper();
 
-$permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('http://okistuff.com/fb-callback.php', $permissions);
+    $permissions = ['email']; // Optional permissions
+    $loginUrl = $helper->getLoginUrl('http://okistuff.com/fb-callback.php', $permissions);
 
-echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+    echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+?>
