d0aee2ea0f8cba693adf54024f654a3b34fc57d6
Stephen Harrington <harrington.st@gmail.com>
2017-06-20 23:31:45 +0900

    finished the page

--- okistuff/header.php
+++ okistuff/header.php
@@ -2,5 +2,7 @@
 <header>
   <a href="./index.php"><img alt="brand" src="./images/logo1.png"></a> 
   <script src='scripts/login.js'></script>
+  <fb:login-button scope="public_profile,email,user_friends" onlogin="checkLoginState();">
+  </fb:login-button>
   <div id="status"></div>
 </header>
